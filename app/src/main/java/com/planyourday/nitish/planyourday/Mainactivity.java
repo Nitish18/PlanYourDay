b35564d0513d542fd1f467a7f012373184560df1
nitish chauhan <nitish.nc18@gmail.com>
2016-02-05 18:31:23 +0530

    more Progress.

--- app/src/main/java/com/planyourday/nitish/planyourday/Mainactivity.java
+++ app/src/main/java/com/planyourday/nitish/planyourday/Mainactivity.java
@@ -2,7 +2,10 @@ package com.planyourday.nitish.planyourday;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 /**
@@ -10,11 +13,31 @@ import android.os.Bundle;
  */
 public class Mainactivity extends Activity {
 
+    Button MAIN_BT1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        MAIN_BT1 = (Button) findViewById(R.id.main_bt1);
+
+        MAIN_BT1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                Intent main_i1 = new Intent(Mainactivity.this,Plantomorrow.class);
+                startActivity(main_i1);
+
+
+            }
+        });
+    }
 
     }
-}
+
+
+
+
+
+
